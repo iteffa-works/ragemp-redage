02a8189ea15878ed7464e8e114c0935aedcb6da7
Микола Довгопол (iTeffa) <97382411+iteffa-works@users.noreply.github.com>
2025-06-21 05:55:49 +0300

    Original - serverside code

---
+++ sources/serverside/NeptuneEvoSDK/Models/Mysql.cs
@@ -0,0 +1,11 @@
+﻿namespace Redage.SDK.Models
+{
+    public class Mysql
+    {
+        public string Name = "MainDB";
+        public string Server = "localhost";
+        public string DataBase = "main";
+        public string User = "root";
+        public string Password = "";
+    }
+}
